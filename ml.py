23452e22b7c165d2ca934717fd96f9b56f3fdcf0
sumit shejole <shejolesumesh@gmail.com>
2026-09-05 11:53:03 +0530

    NEW ADDITION COMMIT add function

--- ml.py
+++ ml.py
@@ -3,3 +3,7 @@ print("Hello, SUMIT")
 print("Before swapping: a =", a, "b =", b)
 a,b = b,a
 print("After swapping: a =", a, "b =", b)
+def add_numbers(x, y):
+    return x + y
+result = add_numbers(5, 3)
+print("The sum is:", result)
