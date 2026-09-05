4fa0c54aaa737dd30d2a1dd96f129ed4a8c9fcfc
aditya48-q <adityagupta86548@gmail.com>
2026-09-05 11:43:58 +0530

    yo

--- ml.py
+++ ml.py
@@ -1,1 +1,5 @@
-print("Hello, SUMIT")
+a,b = map(int, input("Enter two numbers separated by a comma: ").split(','))
+
+print("Before swapping: a =", a, "b =", b)
+a,b = b,a
+print("After swapping: a =", a, "b =", b)
